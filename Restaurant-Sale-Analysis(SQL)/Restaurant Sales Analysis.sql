c5c4086952f2b89ad10f6c84a95cfe2c68df75f4
SkyData <felix.sky.esteban14@gmail.com>
2026-09-21 17:01:50 +0800

    Add SQL queries for restaurant sales analysis

--- Restaurant-Sale-Analysis(SQL)/Restaurant Sales Analysis.sql
+++ Restaurant-Sale-Analysis(SQL)/Restaurant Sales Analysis.sql
@@ -1,1 +1,25 @@
+/*RESTAURANT SALES ANALYSIS USING SQL*/
 
+/*Question1: How many unique orders were placed?*/
+SELECT 
+	COUNT(DISTINCT order_id) AS total_orders
+FROM
+	order_details;
+
+/*Question2: How many order-detail records are there?*/
+SELECT 
+	COUNT(order_details_id) AS total_order_details
+FROM
+	order_details;
+    
+/*Question3: How many different menu items were ordered?*/
+SELECT 
+	COUNT(DISTINCT item_id) AS unique_item_ordered
+FROM
+	order_details;
+    
+/*Question4: What is the total number of items ordered?*/
+SELECT 
+	COUNT(item_id) AS total_items_ordered
+FROM
+	order_details;
